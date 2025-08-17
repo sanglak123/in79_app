89cb9f2afb3b5520ca02a1bf01bc620f987b1329
SangHuynh <sanghuynh.pt91@gmail.com>
2025-08-17 08:44:15 +0700

    config next

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,5 @@
+export default function WrapFooter() {
+    return (
+        <div>Footer</div>
+    )
+}
---
+++ src/components/header/index.tsx
@@ -0,0 +1,5 @@
+export default function WrapHeader() {
+    return (
+        <div>Header</div>
+    )
+}
