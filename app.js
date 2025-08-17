22d52d324fc0f925c9f4027c12036a85ea030a4b
SangHuynh <sanghuynh.pt91@gmail.com>
2025-08-17 17:07:42 +0700

    edit app.js

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const dotenv = require("dotenv");
 dotenv.config();
 const next = require('next');
 
-const dev = process.env.NODE_ENV !== 'production'
-const port = process.env.PORT || 3000
+const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 3000;
 const host = '127.0.0.1' // Passenger proxy được nội bộ, để như vậy là chuẩn
 
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = http.createServer((req, res) => handle(req, res))
+  const server = http.createServer((req, res) => handle(req, res));
   server.listen(port, host, () => {
-    console.log(`> Ready on http://${host}:${port}`)
+    console.log(`> Ready on http://${host}:${port}`);
   })
 })
